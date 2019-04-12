500e5923e57bea0f29adcfab2f9ce418572925fb
JoseUrdiales <jose-urdiales@hotmail.com>
2019-04-12 09:43:49 -0500

    ejemplo2

---
+++ Ejemplo021/src/Paquete2/MiEjemplo3.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Paquete2;
+
+/**
+ *
+ * @author Jose Luis
+ */
+public class MiEjemplo3 {
+    public static void main(String[] args){
+    String nombrePersona;
+    String apellidoPersona;
+    int edad;
+    
+    nombrePersona = "José Luis";
+    apellidoPersona = "Urdiales Cuenca";
+    edad = 20;
+    /**
+    System.out.println("Mi Nombre es: "+nombrePersona+"\n\n\tMi Apellido" 
+            +" es: "+apellidoPersona);
+    */
+    System.out.printf("Mi nombre es: %s\n\n\t Mi apellido es: %s\nMi edad es: %s", 
+            nombrePersona, apellidoPersona, edad);
+    }
+    
+}
